4cbe33d6d95b9bd849db98113085848bad2faeed
Joey--Zeng <jzengstuy@gmail.com>
2014-03-16 03:49:26 -0400

    hw 7b

---
+++ 07b-quicksort/QuickSort.java
@@ -0,0 +1,99 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+
+public class QuickSort{
+    
+    /*
+    public static void quickSort(Integer[] A, int low, int high){
+	int p = (int) (Math.random()*(high-low +1)) + low;
+
+	
+        if (high>low) {
+	    System.out.println("P:"+p + "    H:" + high + "     L:" +low);
+	    ArrayList<Integer> left = new ArrayList<Integer>();
+	    ArrayList<Integer> right = new ArrayList<Integer>();
+	    for(int i = 0; i < p; i++){
+		if(A[i] < A[p])
+		    left.add(A[i]);
+		else
+		    right.add(A[i]);
+	    }
+	    for(int i = p+1; i < A.length; i++){
+		if(A[i] < A[p])
+		    left.add(A[i]);
+		else
+		    right.add(A[i]);
+	    }
+	    
+	left.add(A[p]);
+	left.addAll(right);       
+	A = left.toArray(A);
+	quickSort(A,low,p);
+	quickSort(A,p+1,high);
+
+
+	}
+	}
+
+	*/
+
+    public static void swap(int[] A, int x, int y){
+	int i = A[x];
+	A[x] = A[y];
+	A[y] = i;
+    }
+
+    public static int[] partition (int[] A, int L, int R){
+	int[] retIntA = new int[2];
+	int pivot = A[(int) (Math.random()*(R-L+1)) + L];
+	int wall = L;
+	int wall2 = wall;
+	for (int i = L; i < R+1; i++){
+	    if (A[i] < pivot){
+		swap(A,i,wall);
+		if(wall != wall2)
+		    swap(A,i,wall2);
+		wall++;
+		wall2++;
+	    }
+	    else if (A[i] == pivot){
+		swap(A,i,wall2);
+		wall2++;
+
+	    }
+
+	}	
+	
+	System.out.println(Arrays.toString(A));
+	System.out.println(L+" "+R+" "+wall+" "+wall2);
+	retIntA[0] = wall;
+	retIntA[1] = wall2;
+	return retIntA;
+    }
+    
+    public static void qSort2(int[] A, int L, int R){
+	if (R-L >  0){
+	    int[] p = partition(A,L,R);
+	    qSort2(A,p[1],R); //upper half
+	    qSort2(A,L,p[0]-1); //lower half
+	}
+    }
+    public static void main(String[] args){
+	int n = Integer.parseInt(args[0]);
+	Random rnd = new Random();
+	int[] a = new int[n];
+
+	for (int i=0;i<n;i++)
+	    a[i]= rnd.nextInt(n*3);
+
+	long asdf = System.currentTimeMillis();
+	System.out.println("Before \n" + Arrays.toString(a));
+        qSort2(a, 0, n-1);
+	System.out.println("Sorted \n" + Arrays.toString(a));
+	System.out.println("time :" + (System.currentTimeMillis() - asdf));
+
+
+    }
+}
+
