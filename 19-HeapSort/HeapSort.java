156619be55e99e16dc235ac43306e9234893a00b
Joey--Zeng <jzengstuy@gmail.com>
2014-05-11 17:23:30 -0400

    hw 19

--- 19-HeapSort/HeapSort.java
+++ 19-HeapSort/HeapSort.java
@@ -2,56 +2,108 @@
 public class HeapSort{
     
     
-    public static void heapAdd(Comprable[] Arr, Comprable a){	
-	Arr[count(Arr)]
-	
+    public static void heapAdd(Comparable[] Arr, Comparable a){	
+	Arr[count(Arr)] = a;
+	up(Arr,count(Arr)-1);
     }
     
-    public static int count(Comprable[] Arr){
+    public static int count(Comparable[] Arr){
 	for (int i = Arr.length - 1; i>= 0; i--)
-	    if (a[i] != null)
+	    if (Arr[i] != null)
 		return i+1;
 	return 0;
     }
 
 
-    //adjust indexes
+    
 
-    public static void down(Comprable[] Arr, int index){
-	if( index*2 == count(Arr)){
-	    if (Arr[index*2].compareTo(Arr[index]) > 0){
-		swap(Arr, index*2, index);
-		return down(Arr, index*2);
-	    }
-	    
-	}
-	else if ( index*2 > count(Arr)){
-	    if (Arr[index*2].compareTo(Arr[index*2+1]) > 0){
-		if (Arr[index*2].compareTo(Arr[index]) > 0){
-		    swap(Arr, index*2, index);
-		    return down(Arr, index*2);
+    public static void down(Comparable[] Arr, int index){
+	
+	while (index*2+2 <= count(Arr)){
+	    if (index*2+2 < count(Arr)){
+		if(Arr[index*2+2].compareTo(Arr[index*2+1]) > 0){
+		    if(Arr[index*2+2].compareTo(Arr[index]) > 0){
+			swap(Arr,index*2+2,index);
+			index = index*2+2;			
+		    }
+		    else break;
+		}							
+		else if(Arr[index*2+1].compareTo(Arr[index]) > 0){
+		    swap(Arr,index*2+1,index);
+		    index = index*2+1;
 		}
+		else break;		
 	    }
-	    else
-		if (Arr[index*2+1].compareTo(Arr[index]) > 0){
-		    swap(Arr, index*2+1, index);
-		    return down(Arr, index*2+1);
-		}
-	    
+	    else if (Arr[index*2+1].compareTo(Arr[index]) > 0){
+		swap(Arr,index*2+1,index);
+		index = index*2+1;
+	    }
+	    else break;
 	}
+    }
+    
+    
+    public static Comparable removeMax(Comparable[] Arr){
+	Comparable ret = Arr[0];
+	Arr[0] = null;
+	if (count(Arr) != 0) {
+	    swap(Arr, 0, count(Arr)-1);
+	    down(Arr, 0);
+
+	}
+	return ret;
+    }
+    
+    public static void up(Comparable[] Arr, int index){
+	while(index != 0 && Arr[(index-1)/2].compareTo( Arr[index]) <= 0){
+	    swap(Arr, index, (index-1)/2);
+	    index = (index-1)/2;
+	}
+    }
+    public static void swap(Comparable[] Arr, int i, int i2){
+	Comparable t = Arr[i];
+	Arr[i] = Arr[i2];
+	Arr[i2] = t;
+    }
+
+    public static void heapSort(Comparable[] Arr){
+	Comparable[] copy = new Comparable[count(Arr)];
+
+	for(int i = count(Arr)-1; i >= 0; i--){
+	    heapAdd(copy, Arr[i]);
+	}
+	for(Comparable a: copy){
+	    System.out.print(a+" ");
+	}
+	System.out.println();
+	
+	for(int i = count(Arr)-1; i >= 0; i--){
+
+	    Arr[i] = removeMax(copy);
+	    for(Comparable a: copy){
+		System.out.print(a+" ");
+	    }
+	    System.out.println();
+	}
+	
+	for(Comparable a: Arr){
+	    System.out.print(a+" ");
+	}
+	System.out.println();
 	
     }
     
-    public static void swap(Comprable[] Arr, int i, int i2){
-	Comprable t = a[i];
-	a[i] = a[i2];
-	a[i2] = t;
+
+
+    public static void main(String[] args){
+	Integer[] a = new Integer[10];
+	for(int i = 0; i < 10; i++){
+	    a[i] = (int)(Math.random() * 300);
+	}
+	heapSort(a);
     }
-
-    public static void removeMax(Comprable[] Arr){
-	
-    }
-
-
-
 }
+
+
+
+
