12bf8bf4987f5c73dd639d6f11bf0acd6b4cad96
Joey--Zeng <jzengstuy@gmail.com>
2014-05-18 19:39:56 -0400

    re-add accidentally removed

--- 18-HeapSort/HeapSort.java
+++ 18-HeapSort/HeapSort.java
@@ -1,16 +1,8 @@
-public class Median{
+
+public class HeapSort{
     
-    private int maxNum;
-    private int minNum;
-    private Integer[] maxHeap;
-    private Integer[] minHeap;
     
-    public Median(int a){
-	maxHeap = new Integer[a];
-	minHeap = new Integer[a];
-    }
-    
-    public static void maxHeapAdd(Comparable[] Arr, Comparable a){	
+    public static void heapAdd(Comparable[] Arr, Comparable a){	
 	Arr[count(Arr)] = a;
 	up(Arr,count(Arr)-1);
     }
@@ -21,7 +13,10 @@ public class Median{
 		return i+1;
 	return 0;
     }
-        
+
+
+    
+
     public static void down(Comparable[] Arr, int index){
 	
 	while (index*2+2 <= count(Arr)){
@@ -48,13 +43,6 @@ public class Median{
     }
     
     
-    public static void up(Comparable[] Arr, int index){
-	while(index != 0 && Arr[(index-1)/2].compareTo( Arr[index]) <= 0){
-	    swap(Arr, index, (index-1)/2);
-	    index = (index-1)/2;
-	}
-    }
-
     public static Comparable removeMax(Comparable[] Arr){
 	Comparable ret = Arr[0];
 	Arr[0] = null;
@@ -65,15 +53,57 @@ public class Median{
 	}
 	return ret;
     }
-
+    
+    public static void up(Comparable[] Arr, int index){
+	while(index != 0 && Arr[(index-1)/2].compareTo( Arr[index]) <= 0){
+	    swap(Arr, index, (index-1)/2);
+	    index = (index-1)/2;
+	}
+    }
     public static void swap(Comparable[] Arr, int i, int i2){
 	Comparable t = Arr[i];
 	Arr[i] = Arr[i2];
 	Arr[i2] = t;
     }
 
+    public static void heapSort(Comparable[] Arr){
+	Comparable[] copy = new Comparable[count(Arr)];
 
+	for(int i = count(Arr)-1; i >= 0; i--){
+	    heapAdd(copy, Arr[i]);
+	}
+	for(Comparable a: copy){
+	    System.out.print(a+" ");
+	}
+	System.out.println();
+	
+	for(int i = count(Arr)-1; i >= 0; i--){
 
-}   
-
+	    Arr[i] = removeMax(copy);
+	    for(Comparable a: copy){
+		System.out.print(a+" ");
+	    }
+	    System.out.println();
+	}
+	
+	for(Comparable a: Arr){
+	    System.out.print(a+" ");
+	}
+	System.out.println();
+	
+    }
     
+
+
+    public static void main(String[] args){
+	Integer[] a = new Integer[10];
+	for(int i = 0; i < 10; i++){
+	    a[i] = (int)(Math.random() * 300);
+	}
+	heapSort(a);
+    }
+}
+
+
+
+
